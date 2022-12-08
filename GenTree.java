493b2aae0adea7c6dc7bd62bc96051c61eb16adb
Anatoly <107132732+Seven7897@users.noreply.github.com>
2022-12-08 18:17:02 +0300

    home3

---
+++ GenTree.java
@@ -0,0 +1,18 @@
+package GenealogicalTreeResearch;
+
+import java.util.ArrayList;
+
+public class GenTree implements DataKeeper {
+    private ArrayList<Node> tree = new ArrayList<>();
+
+    @Override
+    public ArrayList<Node> getData() {
+        return tree;
+    }
+
+    @Override
+    public void addData(Human parent, Human child) {
+        tree.add(new Node(parent, Relationship.parent, child));
+        tree.add(new Node(child, Relationship.child, parent));
+    }
+}
